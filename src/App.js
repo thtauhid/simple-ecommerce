cdb537ef43ded9bbb8fee23909a680f213d30aaa
Tauhid <thtauhid.71@gmail.com>
2019-07-04 09:28:25 +0600

    adding components to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from 'react'
 import "./App.css"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Default from './components/Default'
+
+
+
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+      <React.Fragment>
+      	<Navbar />
+      </React.Fragment>
     </div>
-  );
+    );
 }
 
 export default App;
