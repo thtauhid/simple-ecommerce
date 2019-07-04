bc46c0af882b5ecb3808b25fedfd8ec9316b2252
Tauhid <thtauhid.71@gmail.com>
2019-07-04 20:08:00 +0600

    Creating Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
+import logo from '../logo.svg'
 export default class Navbar extends Component {
     render() {
         return (
             <div>
-       			<h3>Hello From Navbar</h3> 		
+       			<nav className="navbar bg-dark navbar-expand-sm navbar-dark px-sm-5">
+       				<Link to="/">
+       					<img src={logo} alt="store" className="navbar-brand" />
+       				</Link>
+       				<ul className="navbar-nav align-iten-center">
+       					<li className="nav-item ml-5">
+       						<Link to="/" className="nav-link">
+       							Products
+       						</Link>
+       					</li>
+       				</ul>
+       				<Link to="/cart" className="ml-auto">
+       					<button className="btn btn-primary">
+       						<i className="fas fa-cart-plus"></i>
+       					</button>
+       				</Link>
+       			</nav>	
         	</div>
         )
     }
