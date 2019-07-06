9fc59671020e9a11ab9bcb8943f8c49319a320e0
Tauhid <thtauhid.71@gmail.com>
2019-07-06 08:28:31 +0600

    For Context API

---
+++ src/components/context.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { storeProducts, detailProduct } from '../data'
+
+const ProductContext = React.createContext()
+//Provider
+//Consumer
+
+class ProductProvider extends Component {
+    state = {
+        products: storeProducts,
+        detailProduct: detailProduct
+    }
+
+    handleDetails = () => {
+        console.log("hello from handleDetails")
+    }
+
+    addToCart = () => {
+        console.log("hello from add to cart")
+    }
+
+    render() {
+        return (
+            <ProductContext.Provider value={{
+            	...this.state,
+    handleDetails: this.handleDetails,
+    addToCart: this.handleDetails
+            }}>
+            	{this.props.children}
+            </ProductContext.Provider>
+        )
+    }
+}
+
+const ProductConsumer = ProductContext.Consumer
+
+export { ProductProvider, ProductConsumer }
